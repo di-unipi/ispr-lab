97e42a54b802873529429b23b0670a3321d4494b
Riccardo Massidda <contact@rmassidda.it>
2025-04-03 14:16:13 +0200

    Run command for Docker/Podman

---
+++ run.sh
@@ -0,0 +1,13 @@
+#! sh
+
+# Default to docker, get flag -p to use podman
+if [ "$1" = "-p" ]; then
+    DOCKER=podman
+else
+    DOCKER=docker
+fi
+
+# build
+$DOCKER build -t intro-tf-keras .
+# run
+$DOCKER run -p 8888:8888 -v $(pwd)/notebooks:/app/notebooks:z -v $(pwd)/_static:/app/_static:z intro-tf-keras
